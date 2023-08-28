e6ccb926cf767897fe9a9c883a680c2ad55d6c9e
Vlad_Rosu <vlad_rosu@dell.com>
2023-08-28 23:16:56 +0000

    Working on config flow validations

--- config_flow.py
+++ config_flow.py
@@ -1,211 +1,198 @@
-"""Config flow for HID Climate Controller integration."""
 from __future__ import annotations
 
 import logging
+import json
 import voluptuous as vol
 
 from typing import Any
+from voluptuous.error import Error
 from homeassistant import config_entries
-from homeassistant.helpers import selector
-from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv, selector
+from homeassistant.helpers.service_info.mqtt import MqttServiceInfo
 from homeassistant.data_entry_flow import FlowResult, AbortFlow
+from json.decoder import JSONDecodeError
 
-from .validations import ValidationException
-from .device_validation import DeviceValidation
 from .const import (
     DOMAIN,
-    USER_FLOW_STEP,
-    CLIMATE_ENTITY_SELECTION_FLOW_STEP,
-    UNIQUE_ID_KEY,
     ENTITY_ID_KEY,
     FRIENDLY_NAME_KEY,
+    DEVICE_UNIQUE_ID_KEY,
+    DEVICE_NAME_KEY,
+    DEVICE_DEFERRED_REGISTRATION_KEY,
     CONTROLLER_KEY,
+    CONTROLLER_ENTITY_ID,
+    CONTROLLER_NAME,
     CLIMATE_KEY,
-    DEFERRED_REGISTRATION_KEY,
+    CLIMATE_ENTITY_ID,
     CLIMATE_ENTITY_TYPE,
+    BASE_ERROR,
+    DEVICE_ALREADY_CONFIGURED_ERROR,
+    MQTT_DISCOVERY_STEP_INVALID_DISCOVERY_PAYLOAD_ERROR,
     MQTT_DISCOVERY_STEP_DEVICE_VALIDATION_FAILURE_ERROR,
     MQTT_DISCOVERY_STEP_UNKNOWN_FAILURE_ERROR,
     USER_INPUT_STEP_UNKNOWN_FAILURE_ERROR,
-    CLIMATE_ENTITY_SELECTION_STEP_NO_AVAILABLE_ENTITIES_ERROR,
-    CLIMATE_ENTITY_SELECTION_STEP_VALIDATION_FAILURE_ERROR,
-    CLIMATE_ENTITY_SELECTION_STEP_UNKNOWN_FAILURE_ERROR,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
+UNIQUE_ID_PATTERN = r"^TC-HID-[A-Za-z0-9]{17}$"
 
-CONTROLLER_CONFIG_SCHEMA = vol.Schema(
+DISCOVERY_CONFIG_SCHEMA = vol.Schema(
     {
-        vol.Required(
-            UNIQUE_ID_KEY
-        ): str  # vol.All(str   DeviceValidation.validate_device_unique_id)
-    }
-)
-
-CLIMATE_LINK_SCHEMA = vol.Schema(
-    {
-        vol.Required(ENTITY_ID_KEY): selector.EntitySelector(
-            selector.EntitySelectorConfig(domain=CLIMATE_ENTITY_TYPE),
+        vol.Required(DEVICE_UNIQUE_ID_KEY): vol.All(
+            cv.string, vol.Match(UNIQUE_ID_PATTERN)
         ),
-    }
+        vol.Optional(DEVICE_NAME_KEY): str,
+    },
+    extra=vol.ALLOW_EXTRA,
 )
 
 
-async def get_all_entities_of_type_as_dict(
-    hass: HomeAssistant, entityType: str
-) -> dict[str, str]:
-    climate_entities = hass.states.async_entity_ids(entityType)
-
-    return {
-        entity_id: hass.states.get(entity_id).attributes.get(
-            FRIENDLY_NAME_KEY, entity_id
-        )
-        for entity_id in climate_entities
-    }
-
-
-async def get_entity_selector_schema(
-    fieldName: str, entities: dict[str, str]
-) -> vol.Schema:
-    return vol.Schema({vol.Required(fieldName): vol.In(entities)})
-
-
-def validate_climate_entity_config(data: dict[str, Any]) -> None:
-    entity_id = data.get(ENTITY_ID_KEY)
-
-    if not entity_id or len(entity_id) == 0:
-        raise ValidationException(
-            CLIMATE_ENTITY_SELECTION_STEP_VALIDATION_FAILURE_ERROR
-        )
+def generate_user_config_schema(data: dict[str, Any]) -> vol.Schema:
+    return vol.Schema(
+        {
+            CONTROLLER_ENTITY_ID: vol.Required(
+                str, default=data.get(DEVICE_UNIQUE_ID_KEY)
+            ),
+            CONTROLLER_NAME: vol.Optional(str, default=data.get(DEVICE_NAME_KEY)),
+            vol.Required(CLIMATE_ENTITY_ID): selector.EntitySelector(
+                selector.EntitySelectorConfig(domain=CLIMATE_ENTITY_TYPE),
+            ),
+        }
+    )
 
 
 @config_entries.HANDLERS.register(DOMAIN)
-class HIDClimateControllerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    _controller_config = None
-    _climate_config = None
+class HidClimateControllerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    _discovery_config = None
 
-    async def async_step_mqtt_discovery(
-        self, data: dict[str, Any] | None = None
+    async def async_step_mqtt(
+        self, discovery_info: MqttServiceInfo | None = None
     ) -> FlowResult:
-        try:
-            DeviceValidation.validate_device(data)
+        unique_id = None
 
-            unique_id = data[UNIQUE_ID_KEY]
+        try:
+            discovery_config = json.loads(discovery_info.payload)
+
+            DISCOVERY_CONFIG_SCHEMA(discovery_config)
+
+            unique_id = discovery_config[DEVICE_UNIQUE_ID_KEY]
 
             await self.async_set_unique_id(unique_id)
             self._abort_if_unique_id_configured()
 
-            data[DEFERRED_REGISTRATION_KEY] = False
+            self._discovery_config = {
+                **discovery_config,
+                DEVICE_UNIQUE_ID_KEY: unique_id,
+                DEVICE_NAME_KEY: discovery_config.get(DEVICE_NAME_KEY),
+                DEVICE_DEFERRED_REGISTRATION_KEY: False,
+            }
 
-            self._controller_config = data
-        except ValidationException as ex:
-            _LOGGER.warning(
-                "Registration failed because of validation errors. Exception: %s. Payload: %s",
+            _LOGGER.info("Triggering user step for %s", self._discovery_config)
+            return await self.async_step_user()
+        except JSONDecodeError as ex:
+            _LOGGER.error(
+                f"MQTT discovery registration failed because of malformed payload. MQTT Payload: {discovery_info.payload}",
                 ex,
-                data,
+            )
+            return self.async_abort(
+                reason=MQTT_DISCOVERY_STEP_INVALID_DISCOVERY_PAYLOAD_ERROR
+            )
+        except Error as ex:
+            _LOGGER.error(
+                f"MQTT discovery registration failed because of validation errors. MQTT Payload: {discovery_info.payload}",
+                ex,
             )
             return self.async_abort(
                 reason=MQTT_DISCOVERY_STEP_DEVICE_VALIDATION_FAILURE_ERROR
             )
         except AbortFlow as ex:
-            raise ex
+            return self.async_abort(
+                reason=DEVICE_ALREADY_CONFIGURED_ERROR,
+                description_placeholders={DEVICE_UNIQUE_ID_KEY: unique_id},
+            )
         except Exception as ex:  # pylint: disable=broad-except
-            _LOGGER.warning("Unknown exception encoutered. Exception: %s", ex)
+            _LOGGER.error("Unknown exception encoutered", ex)
             return self.async_abort(reason=MQTT_DISCOVERY_STEP_UNKNOWN_FAILURE_ERROR)
-
-        return await self.async_step_climate_entity_selection()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
+        unique_id = None
         errors = {}
+
+        if not self._discovery_config:
+            self._discovery_config = {
+                DEVICE_DEFERRED_REGISTRATION_KEY: True,
+            }
 
         if user_input is not None:
             try:
-                DeviceValidation.validate_device(user_input)
-
-                unique_id = user_input[UNIQUE_ID_KEY]
+                unique_id = user_input.get(CONTROLLER_ENTITY_ID)
 
                 await self.async_set_unique_id(unique_id)
                 self._abort_if_unique_id_configured()
 
-                user_input[DEFERRED_REGISTRATION_KEY] = True
+                controller_config = {
+                    ENTITY_ID_KEY: unique_id,
+                    FRIENDLY_NAME_KEY: user_input.get(CONTROLLER_NAME) or unique_id,
+                    DEVICE_DEFERRED_REGISTRATION_KEY: self._discovery_config.get(
+                        DEVICE_DEFERRED_REGISTRATION_KEY, True
+                    ),
+                }
 
-                self._controller_config = user_input
-
-            except ValidationException as ex:
-                _LOGGER.warning(
-                    "Configuration failed because of validation errors. Exception: %s",
-                    ex,
-                )
-                errors["base"] = ex.reason
-            except AbortFlow as ex:
-                raise ex
-            except Exception as ex:  # pylint: disable=broad-except
-                _LOGGER.warning("Unknown exception encoutered. Exception: %s", ex)
-                errors[
-                    USER_INPUT_STEP_UNKNOWN_FAILURE_ERROR
-                ] = USER_INPUT_STEP_UNKNOWN_FAILURE_ERROR
-
-            if len(errors) == 0:
-                return await self.async_step_climate_entity_selection()
-
-        return self.async_show_form(
-            step_id=USER_FLOW_STEP,
-            data_schema=CONTROLLER_CONFIG_SCHEMA,
-            errors=errors,
-        )
-
-    async def async_step_climate_entity_selection(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        errors = {}
-
-        climate_entities = await get_all_entities_of_type_as_dict(
-            self.hass, CLIMATE_ENTITY_TYPE
-        )
-        if len(climate_entities) == 0:
-            return self.async_abort(
-                reason=CLIMATE_ENTITY_SELECTION_STEP_NO_AVAILABLE_ENTITIES_ERROR
-            )
-
-        if user_input is not None:
-            data = None
-
-            try:
-                validate_climate_entity_config(user_input)
-
-                user_input[FRIENDLY_NAME_KEY] = climate_entities.get(
-                    user_input[ENTITY_ID_KEY], user_input[ENTITY_ID_KEY]
+                climate_entity_id = user_input.get(CLIMATE_ENTITY_ID)
+                climate_states = self.hass.states.get(climate_entity_id)
+                climate_friendly_name = climate_states.attributes.get(
+                    FRIENDLY_NAME_KEY, climate_entity_id
                 )
 
-                data = {"controller": self._controller_config, "climate": user_input}
-            except ValidationException as ex:
-                _LOGGER.warning(
-                    "Climate entity selection failed because of validation errors. Exception: %s",
-                    ex,
-                )
-                errors[ex.reason] = ex.reason
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.warning("Unknown exception encoutered. Exception: %s", ex)
-                errors[
-                    CLIMATE_ENTITY_SELECTION_STEP_UNKNOWN_FAILURE_ERROR
-                ] = CLIMATE_ENTITY_SELECTION_STEP_UNKNOWN_FAILURE_ERROR
+                climate_config = {
+                    ENTITY_ID_KEY: climate_entity_id,
+                    FRIENDLY_NAME_KEY: climate_friendly_name,
+                }
 
-            if len(errors) == 0:
+                config = {
+                    CONTROLLER_KEY: controller_config,
+                    CLIMATE_KEY: climate_config,
+                }
+
+                _LOGGER.info("Creating config entry for %s", config)
+
                 return self.async_create_entry(
-                    title=data[CONTROLLER_KEY][UNIQUE_ID_KEY],
-                    description=f"Paired to {data[CLIMATE_KEY][FRIENDLY_NAME_KEY]}",
-                    data=data,
+                    title=config.get(CONTROLLER_KEY, {}).get(FRIENDLY_NAME_KEY),
+                    description=f"Paired to {config.get(CLIMATE_KEY, {}).get(FRIENDLY_NAME_KEY)}",
+                    data=config,
                 )
+            except AbortFlow as ex:
+                return self.async_abort(
+                    reason=DEVICE_ALREADY_CONFIGURED_ERROR,
+                    description_placeholders={CONTROLLER_ENTITY_ID: unique_id},
+                )
+            except Exception as ex:  # pylint: disable=broad-except
+                _LOGGER.error("Unknown exception encoutered", ex)
+                errors[BASE_ERROR] = USER_INPUT_STEP_UNKNOWN_FAILURE_ERROR
 
-        placeholders = {UNIQUE_ID_KEY: self._controller_config[UNIQUE_ID_KEY]}
-
-        schema = await get_entity_selector_schema(ENTITY_ID_KEY, climate_entities)
+        config_schema = generate_user_config_schema(self._discovery_config)
 
         return self.async_show_form(
-            step_id=CLIMATE_ENTITY_SELECTION_FLOW_STEP,
-            description_placeholders=placeholders,
-            data_schema=CLIMATE_LINK_SCHEMA,
+            step_id="user",
+            data_schema=config_schema,
             errors=errors,
         )
+
+
+"""
+Topic: homeassistant/hid_climate_controller/HW-THID-1234567890123/config
+{
+  "name": "HID Climate Controller (HW-THID-1234567890123)",
+  "unique_id": "HW-THID-1234567890123",
+  "device": {
+    "identifiers": ["HW-THID-1234567890123"],
+    "name": "HID Climate Controller (HW-THID-1234567890123)",
+    "model": "HID Climate Controller",
+    "manufacturer": "RedPoint",
+    "sw_version": "1.0"
+  }
+}
+"""
--- validations.py
+++
@@ -1,4 +0,0 @@
-class ValidationException(Exception):
-    def __init__(self, reason="") -> None:
-        super().__init__(reason)
-        self.reason = reason
